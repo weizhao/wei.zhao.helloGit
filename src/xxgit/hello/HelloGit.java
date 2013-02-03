abadcf91d1ec517d3ec6b9bc641bd82a1d190420
Wei Zhao <wei.david.zhao@gmail.com>
2013-02-03 16:00:45 -0500

    c

--- src/xxgit/hello/HelloGit.java
+++ src/xxgit/hello/HelloGit.java
@@ -4,7 +4,7 @@ public class HelloGit {
 
 	public static void main(String [] args){
 		System.out.println("Hello Git!!");
-		//
+		//commit
 		System.out.println("Hello x Git Clone!!");
 		System.out.println("Hello X Git!!");
 	}
